439f56f5042641642b14043cbd3420521fdd435d
Pablissimo <turnacre@hotmail.com>
2013-01-03 17:31:41 +0000

    Version and copyright notices

--- NewRelicConfigBuilder/Properties/AssemblyInfo.cs
+++ NewRelicConfigBuilder/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NewRelicConfigBuilder")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("nrconfig")]
+[assembly: AssemblyDescription("Builds custom instrumentation files for New Relic-instrumented .NET projects")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NewRelicConfigBuilder")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("pablissimo.com")]
+[assembly: AssemblyProduct("nrconfig")]
+[assembly: AssemblyCopyright("Copyright © 2012 Paul O'Neill")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.*")]
