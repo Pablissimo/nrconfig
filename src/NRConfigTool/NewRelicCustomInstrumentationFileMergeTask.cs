f73754d00414e3daed48624970fcf937a827dd3e
Pablissimo <turnacre@hotmail.com>
2013-11-15 21:40:14 +0000

    Worked a way to get dependency CustomInstrumentation.xml files copied through so long as NRConfig build task running in the dependent project

---
+++ src/NRConfigTool/NewRelicCustomInstrumentationFileMergeTask.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRConfigTool
+{
+    public class NewRelicCustomInstrumentationFileMergeTask : AppDomainIsolatedTask
+    {
+        [Required]
+        public string InputFilter
+        {
+            get;
+            set;
+        }
+
+        [Output]
+        public string OutputFile
+        {
+            get;
+            set;
+        }
+
+        public override bool Execute()
+        {
+            this.Log.LogMessage(MessageImportance.High, string.Format("Filter presented: {0}", this.InputFilter));
+
+            return true;
+        }
+    }
+}
